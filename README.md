c774f9416eac6456e4e7537dda48d7b897020e4f
Adam Brodzinski <adambrodzinski@gmail.com>
2014-05-28 16:47:23 -0700

    fix race condition with pause
    pause was not executing fast enough with newer versions of phonegap.
    this calls dis/re/connect directly in the listener to make sure it
    fires in time
    
    *removes auto dis/re/connect on offline/online events*

--- README.md
+++ README.md
@@ -27,3 +27,6 @@ Sets various Cordova events into Session so they can be reactivley used in your 
 
 This package also handles disconnecting before the app is put into background mode. This prevents iOS
 from crashing when app is put into the background and brought back into the foreground.
+
+### note, as of 0.2.0 the package no longer automatically disconnects and reconnects when going online/offline
+
--- cordova-status.js
+++ cordova-status.js
@@ -1,14 +1,15 @@
-// Meteor Generate
+// Cordova Status
 // Copyright(c) 2014 Adam Brodzinski <adambrodzinski@gmail.com>
 // MIT Licensed
 //
 // Set Cordova events into Session so they can be reactivley used in
-// your app. This package also handles disconnecting before the app
-// is put into background mode. This prevents iOS from crashing when
-// app is put into the background
-
-
-/*global cordovaStatus:true */
+// your app.
+//
+// This package also handles disconnecting before the app is put into
+// background mode. This prevents iOS from crashing when paused.
+//
+// If you want to handle offline/online events you'll need to use the
+// reactive online/offline to call #disconnect/#reconnect
 
 document.addEventListener("deviceready", function() {
   Session.set('cordovaReady', true);
@@ -16,36 +17,25 @@ document.addEventListener("deviceready", function() {
   Session.set('cordovaStatus', 'ready');
 });
 
+
 document.addEventListener("pause", function() {
   Session.set('cordovaStatus', 'paused');
+  Meteor.disconnect();
 });
+
 
 document.addEventListener("resume", function() {
   Session.set('cordovaStatus', 'resumed');
+  Meteor.reconnect();
 });
+
 
 document.addEventListener("offline", function() {
   Session.set('cordovaStatus', 'offline');
 });
 
+
 document.addEventListener("online", function() {
   Session.set('cordovaStatus', 'online');
 });
 
-
-// In any of these states, we "may" have lost our connectivity to Meteor
-// if we have not and we are connected, than this command has no effect
-// http://docs.meteor.com/#meteor_reconnect
-//
-Deps.autorun(function () {
-  //console.debug('[Cordova Status]:', cordovaStatus);
-  var cordovaStatus = Session.get('cordovaStatus');
-
-  if (cordovaStatus === 'online' ||  cordovaStatus === 'resumed') {
-    Meteor.reconnect();
-  }
-
-  if (cordovaStatus === 'paused' || cordovaStatus === 'offline') {
-    Meteor.disconnect();
-  }
-});
